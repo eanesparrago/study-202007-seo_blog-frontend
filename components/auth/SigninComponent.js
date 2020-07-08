413c6652e33e10cf21efb607bead2b7070e36784
iansanwich <iansanwich@gmail.com>
2020-07-08 18:30:02 +0800

    Add signin

---
+++ components/auth/SigninComponent.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { signin } from "../../actions/auth";
+import Router from "next/router";
+
+const SigninComponent = () => {
+  const [values, setValues] = useState({
+    email: "alpha@example.com",
+    password: "alpha123",
+    error: "",
+    loading: false,
+    message: "",
+    showForm: true,
+  });
+
+  const { email, password, error, loading, message, showForm } = values;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setValues({ ...values, loading: true, error: false });
+    const user = { email, password };
+
+    signin(user).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        // Save user token to cookie
+        // Save user info to localstorage
+        // Authenticate user
+        Router.push(`/`);
+      }
+    });
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, error: false, [e.target.name]: e.target.value });
+  };
+
+  const signupForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            value={email}
+            name="email"
+            onChange={handleChange}
+            placeholder="Type your email"
+            type="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            value={password}
+            name="password"
+            onChange={handleChange}
+            placeholder="Type your password"
+            type="password"
+            className="form-control"
+          />
+        </div>
+
+        <div>
+          <button className="btn btn-primary">Sign In</button>
+        </div>
+      </form>
+    );
+  };
+
+  return <>{signupForm()}</>;
+};
+
+export default SigninComponent;
--- pages/signin.js
+++ pages/signin.js
@@ -1,13 +1,15 @@
-import Link from "next/link";
 import Layout from "../components/Layout";
+import SigninComponent from "../components/auth/SigninComponent";
 
 const Signin = () => {
   return (
     <Layout>
-      Signin page
-      <Link href="/">
-        <a>Home</a>
-      </Link>
+      <h2 className="text-center pt-4 pb-4">Sign In</h2>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <SigninComponent></SigninComponent>
+        </div>
+      </div>
     </Layout>
   );
 };
