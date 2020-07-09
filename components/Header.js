8378442592cdf4cfd9c1a4b90b47e22f83410836
iansanwich <iansanwich@gmail.com>
2020-07-09 18:58:55 +0800

    Add dashboard link

--- components/Header.js
+++ components/Header.js
@@ -50,6 +50,22 @@ const Header = (props) => {
               </>
             )}
 
+            {isAuth() && isAuth().role === 0 && (
+              <NavItem>
+                <Link href="/user">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
+            {isAuth() && isAuth().role === 1 && (
+              <NavItem>
+                <Link href="/admin">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
             {isAuth() && (
               <NavItem>
                 <NavLink
