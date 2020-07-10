806e59fcbddb6bc55a928a8cc96f41944d696d49
iansanwich <iansanwich@gmail.com>
2020-07-10 17:25:11 +0800

    Add category page

---
+++ pages/admin/crud/category-tag.js
@@ -0,0 +1,28 @@
+import Layout from "../../../components/Layout";
+import Admin from "../../../components/auth/Admin";
+
+const CategoryTag = () => {
+  return (
+    <Layout>
+      <Admin>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 pt-5 pb-5">
+              <h2>Manage Categories and Tags</h2>
+            </div>
+
+            <div className="col-md-6">
+              <p>Categories</p>
+            </div>
+
+            <div className="col-md-6">
+              <p>Tags</p>
+            </div>
+          </div>
+        </div>
+      </Admin>
+    </Layout>
+  );
+};
+
+export default CategoryTag;
