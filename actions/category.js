a6b606ac36e468f5fba8c28d32b7d8a380c62c83
iansanwich <iansanwich@gmail.com>
2020-07-10 18:27:22 +0800

    Add category read, delete, and messages

--- actions/category.js
+++ actions/category.js
@@ -17,3 +17,38 @@ export const create = (category, token) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const getCategories = () => {
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getSingleCategory = (slug) => {
+  return fetch(`${API}/category/${slug}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const removeCategory = (slug, token) => {
+  return fetch(`${API}/category/${slug}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
--- components/crud/Category.js
+++ components/crud/Category.js
@@ -1,6 +1,6 @@
-import { useState, useForm } from "react";
+import { useState, useEffect } from "react";
 import { getCookie } from "../../actions/auth";
-import { create } from "../../actions/category";
+import { create, getCategories, removeCategory } from "../../actions/category";
 
 const Category = () => {
   const [values, setValues] = useState({
@@ -9,10 +9,65 @@ const Category = () => {
     success: false,
     categories: [],
     removed: false,
+    reload: false,
   });
 
-  const { name, error, success, categories, removed } = values;
+  const { name, error, success, categories, removed, reload } = values;
   const token = getCookie("token");
+
+  useEffect(() => {
+    loadCategories();
+  }, [reload]);
+
+  const loadCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setValues({ ...values, categories: data });
+      }
+    });
+  };
+
+  const showCategories = () => {
+    return categories.map((category, index) => (
+      <button
+        onDoubleClick={() => promptDelete(category.slug)}
+        title="Double click to delete"
+        key={index}
+        className="btn btn-outline-primary mr-1 ml-1 mt-3"
+      >
+        {category.name}
+      </button>
+    ));
+  };
+
+  const promptDelete = (slug) => {
+    let answer = window.confirm(
+      "Are you sure you want to delete this category?"
+    );
+
+    if (answer) {
+      deleteCategory(slug);
+    }
+  };
+
+  const deleteCategory = (slug) => {
+    removeCategory(slug, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setValues({
+          ...values,
+          error: false,
+          success: false,
+          name: "",
+          removed: true,
+          reload: !reload,
+        });
+      }
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,7 +76,14 @@ const Category = () => {
       if (data.error) {
         setValues({ ...values, error: data.error, success: false });
       } else {
-        setValues({ ...values, error: false, success: true, name: "" });
+        setValues({
+          ...values,
+          error: false,
+          success: true,
+          name: "",
+          removed: false,
+          reload: !reload,
+        });
       }
     });
   };
@@ -32,8 +94,26 @@ const Category = () => {
       name: e.target.value,
       error: false,
       success: false,
-      removed: "",
+      removed: false,
     });
+  };
+
+  const showSuccess = () => {
+    if (success) {
+      return <p className="text-success">Category is created</p>;
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <p className="text-danger">Category already exist</p>;
+    }
+  };
+
+  const showRemoved = () => {
+    if (removed) {
+      return <p className="text-success">Category is removed</p>;
+    }
   };
 
   const newCategoryForm = () => (
@@ -58,7 +138,16 @@ const Category = () => {
     </form>
   );
 
-  return <>{newCategoryForm()}</>;
+  return (
+    <>
+      {showSuccess()}
+      {showError()}
+      {showRemoved()}
+
+      {newCategoryForm()}
+      <div>{showCategories()}</div>
+    </>
+  );
 };
 
 export default Category;
