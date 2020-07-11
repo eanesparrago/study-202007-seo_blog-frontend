821f5209ed6fe638f36fabfef3d4429fffe9332c
iansanwich <iansanwich@gmail.com>
2020-07-11 18:39:02 +0800

    Add blog create action and imports

--- components/crud/BlogCreate.js
+++ components/crud/BlogCreate.js
@@ -1,6 +1,17 @@
-import React from "react";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import Router from "next/router";
+import dynamic from "next/dynamic";
+import { withRouter } from "next/router";
+import { getCookie, isAuth } from "../../actions/auth";
+import { getCategories } from "../../actions/category";
+import { getTags } from "../../actions/tag";
+import { createBlog } from "../../actions/blog";
 
-const BlogCreate = () => {
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
+
+const BlogCreate = ({ router }) => {
   return (
     <div>
       <h2>Blog Create</h2>
@@ -8,4 +19,4 @@ const BlogCreate = () => {
   );
 };
 
-export default BlogCreate;
+export default withRouter(BlogCreate);
