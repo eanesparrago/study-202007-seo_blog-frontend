4798d0ae616d52d9bf84b4df4ed1133c57828b45
iansanwich <iansanwich@gmail.com>
2020-07-12 11:37:35 +0800

    Show categories and tags checkboxes

--- components/crud/BlogCreate.js
+++ components/crud/BlogCreate.js
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import Router from "next/router";
@@ -25,6 +24,9 @@ const BlogCreate = ({ router }) => {
     }
   };
 
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+
   const [body, setBody] = useState(blogFromLs());
   const [values, setValues] = useState({
     error: "",
@@ -46,7 +48,29 @@ const BlogCreate = ({ router }) => {
 
   useEffect(() => {
     setValues({ ...values, formData: new FormData() });
+    initCategories();
+    initTags();
   }, [router]);
+
+  const initCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  const initTags = () => {
+    getTags().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setTags(data);
+      }
+    });
+  };
 
   const publishBlog = (e) => {
     e.preventDefault();
@@ -74,6 +98,24 @@ const BlogCreate = ({ router }) => {
       localStorage.setItem("blog", JSON.stringify(e));
     }
   };
+
+  const showCategories = () =>
+    categories &&
+    categories.map((category, i) => (
+      <li className="list-unstyled" key={i}>
+        <input type="checkbox" className="mr-2" />
+        <label className="form-check-label">{category.name}</label>
+      </li>
+    ));
+
+  const showTags = () =>
+    tags &&
+    tags.map((tag, i) => (
+      <li className="list-unstyled" key={i}>
+        <input type="checkbox" className="mr-2" />
+        <label className="form-check-label">{tag.name}</label>
+      </li>
+    ));
 
   const createBlogForm = () => (
     <form onSubmit={publishBlog}>
@@ -106,7 +148,45 @@ const BlogCreate = ({ router }) => {
     </form>
   );
 
-  return <div>{createBlogForm()}</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-8">
+          {createBlogForm()}
+          <hr />
+          {JSON.stringify(title)}
+          <hr />
+          {JSON.stringify(body)}
+          <hr />
+          {JSON.stringify(categories)}
+          <hr />
+          {JSON.stringify(tags)}
+        </div>
+
+        <div className="col-md-4">
+          <div>
+            <h5>Categories</h5>
+
+            <hr />
+
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showCategories()}
+            </ul>
+          </div>
+
+          <div>
+            <h5>Tags</h5>
+
+            <hr />
+
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showTags()}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 BlogCreate.modules = {
