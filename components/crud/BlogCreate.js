a8778b2100acc49d48502cddbc7d421f53f5335e
iansanwich <iansanwich@gmail.com>
2020-07-12 10:30:33 +0800

    Add blog create form

--- components/crud/BlogCreate.js
+++ components/crud/BlogCreate.js
@@ -12,11 +12,66 @@ const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "react-quill/dist/quill.snow.css";
 
 const BlogCreate = ({ router }) => {
-  return (
-    <div>
-      <h2>Blog Create</h2>
-    </div>
+  const [body, setBody] = useState({});
+  const [values, setValues] = useState({
+    error: "",
+    sizeError: "",
+    success: "",
+    formData: "",
+    title: "",
+    hidePublishButton: false,
+  });
+
+  const {
+    error,
+    sizeError,
+    success,
+    formData,
+    title,
+    hidePublishButton,
+  } = values;
+
+  const publishBlog = (e) => {
+    e.preventDefault();
+    console.log("Ready to publish blog");
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleBody = () => {};
+
+  const createBlogForm = () => (
+    <form onSubmit={publishBlog}>
+      <div className="form-group">
+        <label className="text-muted">Title</label>
+        <input
+          type="text"
+          className="form-control"
+          value={title}
+          name="title"
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <ReactQuill
+          value={body}
+          placeholder="Write something amazing..."
+          onChange={handleBody}
+        ></ReactQuill>
+      </div>
+
+      <div>
+        <button className="btn btn-primary" type="submit">
+          Publish
+        </button>
+      </div>
+    </form>
   );
+
+  return <div>{createBlogForm()}</div>;
 };
 
 export default withRouter(BlogCreate);
