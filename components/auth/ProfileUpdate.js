f54f30059127687a838956d224fe6e41fafedc4c
iansanwich <iansanwich@gmail.com>
2020-07-15 19:21:41 +0800

    Add update user in local storage

--- components/auth/ProfileUpdate.js
+++ components/auth/ProfileUpdate.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import Router from "next/router";
-import { getCookie, isAuth } from "../../actions/auth";
+import { getCookie, isAuth, updateUser } from "../../actions/auth";
 import { getProfile, update } from "../../actions/user";
 import { API } from "../../config";
 
@@ -84,6 +84,18 @@ const ProfileUpdate = () => {
           loading: false,
         });
       } else {
+        updateUser(data, () => {
+          setValues({
+            ...values,
+            username: data.username,
+            name: data.name,
+            email: data.email,
+            about: data.about,
+            success: true,
+            loading: false,
+          });
+        });
+
         setValues({
           ...values,
           username: data.username,
