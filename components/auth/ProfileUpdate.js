078806bc945a72db543b7b2b67825fbdb483ee65
iansanwich <iansanwich@gmail.com>
2020-07-15 18:14:02 +0800

    Setup profile update component

--- components/auth/ProfileUpdate.js
+++ components/auth/ProfileUpdate.js
@@ -1,7 +1,69 @@
-import React from "react";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import Router from "next/router";
+import { getCookie, isAuth } from "../../actions/auth";
+import { getProfile, update } from "../../actions/user";
 
 const ProfileUpdate = () => {
-  return <div>Profile Update</div>;
+  const [values, setValues] = useState({
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+    error: false,
+    success: false,
+    loading: false,
+    photo: "",
+    userData: "",
+  });
+
+  const token = getCookie("token");
+  const {
+    username,
+    name,
+    email,
+    password,
+    error,
+    success,
+    loading,
+    photo,
+    userData,
+  } = values;
+
+  const init = () => {
+    getProfile(token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({
+          ...values,
+          username: data.username,
+          name: data.name,
+          email: data.email,
+          about: data.about,
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">image</div>
+
+          <div className="col-md-8">
+            Update form
+            {JSON.stringify({ username, email, name })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProfileUpdate;
