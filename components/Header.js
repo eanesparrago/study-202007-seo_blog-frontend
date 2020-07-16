7966e2a506ffe0582f2efaca1cb6a792b2168488
iansanwich <iansanwich@gmail.com>
2020-07-16 13:05:43 +0800

    Setup block create for auth user

--- components/Header.js
+++ components/Header.js
@@ -92,6 +92,14 @@ const Header = (props) => {
                 </NavLink>
               </NavItem>
             )}
+
+            <NavItem>
+              <Link href="/user/crud/create">
+                <NavLink className="btn btn-primary text-light">
+                  Write a blog
+                </NavLink>
+              </Link>
+            </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
